8b0ae8140e4c78cf9045c3142ed3ae8d7ad0d2f8
C4m0mila <112404881+C4m0mila@users.noreply.github.com>
2022-11-07 20:37:10 -0300

    update login & recuperação

--- js/recuperacao.js
+++ js/recuperacao.js
@@ -1,10 +1,22 @@
 function validar(){
     var email = document.getElementById("email");
+    var alert = document.querySelector(".alert");
 
     if (email.value == ""){
-        alert("E-mail não informado");
+        alert.style.display='block';
         email.focus();
         return;
     }
-    alert("Mensagem Enviada");
 }
+
+// function validar(){
+//     var email = document.getElementById("email");
+//     var form = document.querySelector('form')
+//     var sent = document.getElementById('sent')
+
+//    if (email.value == '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/'){
+//   return form.style.display='none';
+//    }else if (form.style.display='none'){
+//     return sent.style.display='block';
+//    }
+// }
